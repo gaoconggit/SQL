33ff48f379b7605d4cd1f9d7a3c515360951d15b
gaoconggit <718497701@qq.com>
2015-04-23 11:13:54 +0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -13,3 +13,11 @@ SELECT * FROM a;
 SELECT * FROM b
 SELECT * FROM a
 UPDATE ab SET ab.name=ac.height FROM a AS ab ,b AS ac WHERE ab.age=ac.height
+
+--加列
+alter   table   表名   add   列名   数据类型
+如：alter   table   student   add   nickname   char(20)
+
+--加约束
+alter table grade
+add constraint id_fk  foreign key (id) references student (id)
